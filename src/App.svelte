93e67f53c2df8fe0cd5d61331c2fef7e02a8e1d6
Silvio Pilato <pilatosil@gmail.com>
2023-02-12 22:14:29 +0100

    Improve searchbar style

--- src/App.svelte
+++ src/App.svelte
@@ -70,7 +70,7 @@
 
 <main class="bg-sky-50 dark:bg-slate-900">
     <div class="flex flex-col items-center">
-        <div class="w-fit">
+        <div class="w-fit flex flex-col gap-4">
             <div class="flex justify-between w-full">
                 <h1 class="text-3xl font-semibold">GIF APP</h1>
                 <span>
--- src/lib/Searchbar.svelte
+++ src/lib/Searchbar.svelte
@@ -9,19 +9,41 @@
 </script>
 
 <div class="flex w-full">
-    <form class="w-full" on:submit|preventDefault={onFormSubmit}>
-        <input autocomplete="off" type="text" class="
-            h-12 
-            w-full
-            font-sans
-            text-xl
-            rounded-full
-            pl-4
-            outline
-            outline-2 
-            outline-pink-200
-            focus:outline-pink-400
-            " name="gif-search" id="gif-search" bind:value={search} />
-        <Fa icon={faSearch} />
+    <form class="group flex w-full" on:submit|preventDefault={onFormSubmit}>
+        <input autocomplete="off" placeholder="Search all the GIFs" type="text" class="
+                h-12 
+                font-sans
+                text-xl
+                w-11/12
+                rounded-tl-full
+                rounded-bl-full
+                pl-4
+                outline
+                outline-2
+                flex-grow-12
+                outline-pink-200
+                focus:outline-pink-300
+                hover:outline-pink-300
+                group-hover:outline-pink-300
+                
+            "
+            name="gif-search" id="gif-search" bind:value={search} />
+            <button class="
+                bg-pink-200
+                outline
+                outline-2 
+                outline-pink-200 
+                w-1/12
+                flex-grow 
+                rounded-tr-full 
+                rounded-br-full
+                flex
+                justify-center
+                items-center
+                group-hover:outline-pink-300
+                group-hover:bg-pink-300
+                " 
+            type="submit"
+            ><Fa color={"#fafafa"} scale={1.5} icon={faSearch} /></button>
     </form>
 </div>
